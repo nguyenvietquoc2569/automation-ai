94771a7ec2383368c5791153a40686ac5fcb753d
Harry Nguyen <nvquoc.uit@gmail.com>
2025-06-15 13:04:09 -0500

    add service. user-auth

---
+++ libs/feature/user-auth/services/src/lib/services.tsx
@@ -0,0 +1,11 @@
+import styles from './services.module.css';
+
+export function AutomationAiServices() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to AutomationAiServices!</h1>
+    </div>
+  );
+}
+
+export default AutomationAiServices;
