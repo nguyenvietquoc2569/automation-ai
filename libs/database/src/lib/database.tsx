e1adb6a0e659f5749b4bcbaf22f1cda5bcc3acce
Harry Nguyen <nvquoc.uit@gmail.com>
2025-06-15 10:14:39 -0500

    add dâtbase project

---
+++ libs/database/src/lib/database.tsx
@@ -0,0 +1,11 @@
+import styles from './database.module.css';
+
+export function Database() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Database!</h1>
+    </div>
+  );
+}
+
+export default Database;
